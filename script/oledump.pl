59a644ce173b10e79f6e30fb2dc8e660b13f8565
Matijs van Zuijlen <matijs@matijs.net>
2014-09-02 18:15:18 +0200

    Use only spaces for indening oledump.pl

--- script/oledump.pl
+++ script/oledump.pl
@@ -11,15 +11,15 @@
 #
 # Changes made by Matijs van Zuijlen:
 #   2002.04.16:
-#	Indenting
-#	Added #! line.
-#	Moved main of program to end.
-#	English
-#	Removed prototype info from PrnItem, awaiting knowledge on how to
-#	resolve warnings about it.
-#	Added code to print data as well.
+#       Indenting
+#       Added #! line.
+#       Moved main of program to end.
+#       English
+#       Removed prototype info from PrnItem, awaiting knowledge on how to
+#       resolve warnings about it.
+#       Added code to print data as well.
 #   2002.04.17:
-#	Allow more characters as printable.
+#       Allow more characters as printable.
 #
 #=================================================================
 
@@ -42,63 +42,63 @@ sub PrnItem {
     # Make Name (including PPS-no and level)
     my $sName = OLE::Storage_Lite::Ucs2Asc($oPps->{Name});
     $sName =~ s/\W/ /g;
-    $sName = sprintf("%s %3d '%s' (pps %x)", 
-	    ' ' x ($iLvl * 2), $iDir, $sName, $oPps->{No});
+    $sName = sprintf("%s %3d '%s' (pps %x)",
+            ' ' x ($iLvl * 2), $iDir, $sName, $oPps->{No});
 
-    # Make Date 
+    # Make Date
     my $sDate;
     if($oPps->{Type}==2) {
-	$sDate = sprintf("%10x bytes", $oPps->{Size});
+        $sDate = sprintf("%10x bytes", $oPps->{Size});
     }
     else {
-	$raDate = $oPps->{Time2nd};
-	$raDate = $oPps->{Time1st} unless($raDate);
-	$sDate = ($raDate)?
-	    sprintf("%02d.%02d.%4d %02d:%02d:%02d", 
-		    $raDate->[3], $raDate->[4]+1, $raDate->[5]+1900,
-		    $raDate->[2], $raDate->[1],   $raDate->[0]) : "";
+        $raDate = $oPps->{Time2nd};
+        $raDate = $oPps->{Time1st} unless($raDate);
+        $sDate = ($raDate)?
+            sprintf("%02d.%02d.%4d %02d:%02d:%02d",
+                    $raDate->[3], $raDate->[4]+1, $raDate->[5]+1900,
+                    $raDate->[2], $raDate->[1],   $raDate->[0]) : "";
     }
 
     # Display
-    printf "%02d %-50s %-4s %s\n", 
-	${$iTtl}++,
-	$sName,
-	$sPpsName{$oPps->{Type}},
-	$sDate;
-    
+    printf "%02d %-50s %-4s %s\n",
+        ${$iTtl}++,
+        $sName,
+        $sPpsName{$oPps->{Type}},
+        $sDate;
+
     # MvZ: Print Data
     if ($prData and $oPps->{Type}==2 and $oPps->{Size} > 0) {
-	my $data = $oPps->{Data};
-	my $length = length($data);
-	my $numloops = $length/16;
-	my $i;
+        my $data = $oPps->{Data};
+        my $length = length($data);
+        my $numloops = $length/16;
+        my $i;
 
-	for ($i=0; $i<$numloops; $i++) {
-	    #print "$i; $numloops;\n";
-	    my $substring = substr($data, $i*16, 16);
-	    my $copy = $substring;
-	    $substring =~ s/./sprintf("%02x ", ord($&))/sge;
-	    $copy =~ s/[^[:print:]]/./sg;
-	    #$copy =~ s/[\x00-\x1f\x7f-\xa0]/./sg;
-	    #$copy =~ s/[\x09\x0a\x0c\x0d]/./sg;
+        for ($i=0; $i<$numloops; $i++) {
+            #print "$i; $numloops;\n";
+            my $substring = substr($data, $i*16, 16);
+            my $copy = $substring;
+            $substring =~ s/./sprintf("%02x ", ord($&))/sge;
+            $copy =~ s/[^[:print:]]/./sg;
+            #$copy =~ s/[\x00-\x1f\x7f-\xa0]/./sg;
+            #$copy =~ s/[\x09\x0a\x0c\x0d]/./sg;
 
-	    print " " x 12;
-	    print sprintf("%-48s %-16s\n", $substring, $copy);
-	}
+            print " " x 12;
+            print sprintf("%-48s %-16s\n", $substring, $copy);
+        }
     }
 
     # For its Children
     my $iDirN=1;
     foreach my $iItem (@{$oPps->{Child}}) {
-	PrnItem($iItem, $iLvl+1, $iTtl, $iDirN, $prData);
-	$iDirN++;
+        PrnItem($iItem, $iLvl+1, $iTtl, $iDirN, $prData);
+        $iDirN++;
     }
 }
 
 # Main
 #
 my $prData;
-my $help = '';	    # Print help message and exit.
+my $help = '';      # Print help message and exit.
 my $opt = GetOptions("with-data" => \$prData) or pod2usage(2);
 pod2usage(1) if $help;
 pod2usage(2) if($#ARGV < 0);
@@ -123,8 +123,8 @@ oledump.pl - Dump structure of an OLE file.
 oledump.pl [options] <file>...
 
   Options:
-    --with-data	    dump data too
-    --help	    help message
+    --with-data     dump data too
+    --help          help message
 
 =head1 OPTIONS
 
