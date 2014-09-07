a2261b3b2b886f0210bb687f5eef8a57cef27ace
Aitor Pazos <mail@aitorpazos.es>
2014-09-07 14:55:20 +0200

    --open and --openwith options allows better desktop integration on several OSs. --openwith option implies --open. Temp files are created using File::Temp, which allows to support multiple OSs when no --destfolder parameter is provided. Parameters handling have been improved.

--- script/msgconvert.pl
+++ script/msgconvert.pl
@@ -8,27 +8,36 @@
 use Config;
 use Email::Outlook::Message;
 use Email::Sender::Transport::Mbox;
-use Getopt::Long;
+use Getopt::Long qw(:config no_auto_abbrev bundling);
 use Pod::Usage;
 use File::Basename;
+use File::Temp qw/ tempfile /;
 use vars qw($VERSION);
-$VERSION = "0.904";
-my $XDG_OPEN_BIN = "/usr/bin/xdg-open";
+$VERSION = "0.905";
+
+my $XDG_OPEN_BIN = '';
+# Find standard xdg-open on UNIX-like systems
+if ($Config{osname} ne 'darwin|MSWin32|cygwin'){
+    my $xdgOpenPath = `which xdg-open`;
+    chomp($xdgOpenPath);
+	$XDG_OPEN_BIN = $xdgOpenPath;
+}
+
 
 # Setup command line processing.
 my $verbose = '';
 my $mboxfile = '';
 my $destfolder = '';
 my $open = '';
-my $openwith = $XDG_OPEN_BIN;
+my $openwith = '';
 my $help = '';	    # Print help message and exit.
 GetOptions(
-  'destfolder=s' => \$destfolder,
-  'mbox=s' => \$mboxfile,
-  'open' => \$open,
-  'openwith' => \$openwith,
-  'verbose' => \$verbose,
-  'help|?' => \$help) or pod2usage(2);
+  'd|destfolder=s' => \$destfolder,
+  'm|mbox=s' => \$mboxfile,
+  'o|open' => \$open,
+  'w|openwith=s' => \$openwith,
+  'v|verbose' => \$verbose,
+  'h|help|?' => \$help) or pod2usage(2);
 pod2usage(1) if $help;
 
 # Check file names
@@ -49,22 +58,43 @@ foreach my $file (@ARGV) {
   } else {
     my $basename = basename($file, qr/\.msg/i);
     my $outfile = "$basename.eml";
-    
-    # If destfolder is used --open parameter is ignored
-    if ($destfolder ne '') {
-      $outfile = File::Spec->catfile($destfolder, "$basename.mime");
-    } elsif ($open ne '' && $Config{osname} eq 'linux') { # Only used under linux
-      $outfile = File::Spec->catfile("/tmp/", "$basename.mime");
+    my $basepath = '';
+
+    # If no --destfolder is specified and --open or --openwith is used, 
+    # OS dependant tempFile will be used.
+    if (($open ne '' || $openwith ne '') && $destfolder eq '') {
+        ($tf, $outfile)=tempfile();
+        close $tf;
+        $outfile="$outfile.eml";
+    } else { 
+        if ($destfolder ne '') {
+            $basepath=$destfolder;
+        } else {
+            $basepath=".";
+        }
+        $outfile = File::Spec->catfile($basepath, "$basename.eml");
     }
+    
     open OUT, ">:utf8", $outfile
       or die "Can't open $outfile for writing: $!";
     binmode(OUT, ":utf8");
     print OUT $mail->as_string;
     close OUT;
-    
+
     # File is opened
-    if ($open ne '' && $Config{osname} eq 'linux') { # Only used under linux
-      system($openwith, $outfile);
+    my $customFileOpen = $XDG_OPEN_BIN;
+    # First we need to find the correct way of launching generated mail on each
+    # OS
+    if($Config{osname} eq 'darwin' && $openwith eq '') { # MacOS
+        $customFileOpen='open';
+    } elsif ($Config{osname} eq 'MSWin32|cygwin' && $openwith eq ''){ #Win
+        $customFileOpen='start';
+    } elsif ($openwith ne '') {
+        $customFileOpen=$openwith;
+    }
+    # Launching file if --open or --openwith is have been used
+    if ($open ne '' || $openwith ne ''){
+        system($customFileOpen, $outfile);
     }
   }
 }
@@ -83,55 +113,59 @@ msgconvert.pl - Convert Outlook .msg files to mbox format
 msgconvert.pl [options] <file.msg>...
 
   Options:
-    --destfolder <folder>   folder where generated file will be placed
-    --mbox <file>           deliver messages to mbox file <file>
-    --open                  calls system's default program to open de converted file.
-    --openwith              especify program to be used in order to open generated file.
-    --verbose	            be verbose
-    --help	                help message
+    -d, --destfolder=FOLDER     folder where generated file will be placed
+    -m, --mbox=FILE             deliver messages to mbox file <file>
+    -o, --open                  calls system's default program to open de converted file.
+    -w, --openwith=PROGRAM_PATH especify program to be used in order to open generated file.
+    -v, --verbose               be verbose
+    -h, --help                  help message
 
 =head1 OPTIONS
 
 =over 8
 
-=item B<--destfolder>
+=item B<-d, --destfolder>
 
-    Place generated .mime files into especified folder.
+    Place generated .eml files into especified folder.
 
-=item B<--mbox>
+=item B<-m, --mbox>
 
-    Deliver to the given mbox file instead of creating individual .mime
+    Deliver to the given mbox file instead of creating individual .eml
     files.
 
-=item B<--open>
+=item B<-o, --open>
 
-    Calls /usr/bin/xdg-open (unless --openwith is used) once .mime file is 
-    generated.
+    Calls open,start or xdg-open (depending on OS) unless --openwith is used 
+    once .eml file is generated.
 
-=item B<--openwith>
+=item B<-w, --openwith>
 
-    Allows to define which program will be called after .mime file generation.
+    Allows to define which program will be called after .eml file generation.
     
-=item B<--verbose>
+=item B<-v, --verbose>
 
     Print information about skipped parts of the .msg file.
 
-=item B<--help>
+=item B<-h, --help>
 
     Print a brief help message.
 
 =head1 DESCRIPTION
 
 This program will convert the messages contained in the Microsoft Outlook
-files <file.msg>...  to message/rfc822 files with extension .mime.
+files <file.msg>...  to message/rfc822 files with extension .eml.
 Alternatively, if the --mbox option is present, all messages will be put in
 the given mbox file.  This program will complain about unrecognized OLE
 parts in the input files on stderr.
 
+--open and --openwith parameters allows to integrate it into desktop
+environments and use it to open generated file directly with your favourite 
+application.
+
 =head1 BUGS
 
 The program will not check whether output files already exist. Also, if you
-feed it "foo.MSG" and "foo.msg", you'll end up with one "foo.mime",
+feed it "foo.MSG" and "foo.msg", you'll end up with one "foo.eml",
 containing one of the messages.
 
 Not all data that's in the .MSG file is converted. There simply are some
